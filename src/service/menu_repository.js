542f9abd0b0082db12f4ee725eb9ed60cd73a800
mjungp <mjungpp@naver.com>
2022-05-18 20:54:08 +0900

    warning 수정

--- src/service/menu_repository.js
+++ src/service/menu_repository.js
@@ -1,15 +1,12 @@
-import firebaseApp from './firebase';
+import { firebaseDatabase } from './firebase';
 
 class MenuRepository {
     saveMenu(userId, menu) {
-        firebaseApp
-        .database()
-        .ref(`${userId}/menus/${menu.id}`)
-        .set(menu);
+       firebaseDatabase.ref(`${userId}/menus/${menu.id}`).set(menu);
     }
 
     syncMenus(userId, onUpdate) {
-        const ref = firebaseApp.database().ref(`${userId}/menus`);
+        const ref =firebaseDatabase.ref(`${userId}/menus`);
         ref.on('value', snapshot => {
             const value = snapshot.val();
             value && onUpdate(value);
