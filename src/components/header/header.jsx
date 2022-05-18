04350e4828294b18efbe624cf61340b6bfb0c768
mjungp <mjungpp@naver.com>
2022-05-18 15:37:27 +0900

    header issue resolution

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import styles from './header.module.css';
 
 const Header = ({ onLogout }) => (
-    <header header className={styles.header} >
-        {
-        onLogout && (<button className={styles.logout} onClick={onLogout}>Logout</button>)
-        }
+    <header className={styles.header} >
+            {
+                onLogout && (
+                    <button className={styles.logout} onClick={onLogout}>Logout</button>
+                )
+            }
         <img className={styles.logo} src="/images/logo.png" alt="logo image" />
         <h1>STARBUCKS Online Ordering App</h1>
     </header >
 )
-
 export default Header;
